ba6fa00be4be5bacaaf6f1e239aa041ceeee6710
bonnieives <120677106+bonnieives@users.noreply.github.com>
2023-09-20 18:41:03 -0400

    Add files via upload

---
+++ question5.py
@@ -0,0 +1,17 @@
+"""
+
+    ASSIGNMENT #1
+    QUESTION 5
+    AUTHOR: Bonnie Ives de Castro Nunes
+    DATE: September 15th, 2023
+    
+"""
+
+newList = [1,2,3,4,5,6,7]
+listLength = len(newList)
+print(newList[0] , " is " , newList[listLength-1] , " : ", int(newList[0]) is int(newList[listLength-1]))    
+
+
+newList = [1,2,3,4,5,6,7,6,5,4,3,2,1]
+listLength = len(newList)
+print(newList[0] , " is " , newList[listLength-1] , " : ", int(newList[0]) is int(newList[listLength-1]))
